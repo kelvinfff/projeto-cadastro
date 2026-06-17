f4df11970fef538ae5cfb1b06a55a6d05c0e102d
kelvinfff <167117708+kelvinfff@users.noreply.github.com>
2026-06-17 16:28:09 -0300

    Subindo código frontend e cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const database = require("./config/database")
 const routes = require("./routes/produtoRoute")
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.json())
 
 app.use(routes)
+app.use(cors())
 
 app.listen(3001,()=>{
     console.log("Servidor rodando...")
